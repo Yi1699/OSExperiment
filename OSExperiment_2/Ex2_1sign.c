34eba9682e5f5c9bcc0b94c0ef192a170f35515e
SuperYeeEuler <1337936093@qq.com>
2023-11-06 23:04:11 +0800

    finish the Ex2_1sign

--- OSExperiment_2/Ex2_1sign.c
+++ OSExperiment_2/Ex2_1sign.c
@@ -16,7 +16,7 @@ void waiting()
 }
 int main() {
     pid_t pid1=-1, pid2=-1;
-    signal(2, inter_handler);
+    signal(3, inter_handler);
     while (pid1 == -1) pid1 = fork();
     if (pid1 > 0) {
         while (pid2 == -1) pid2 = fork();
@@ -33,20 +33,20 @@ int main() {
         } 
         else {
             // TODO: 子进程 2
-            signal(2, SIG_IGN);
+            signal(3, SIG_IGN);
             flag = 1;
-            waiting();
             signal(17, inter_handler);
+	    waiting();
             printf("\nChild process2 is killed by parent!!\n");
             return 0;
         }
     } 
     else {
         // TODO：子进程 1
-        signal(2, SIG_IGN);
+        signal(3, SIG_IGN);
         flag = 1;
-        waiting();
         signal(16, inter_handler);
+	waiting();
         printf("\nChild process1 is killed by parent!!\n");
         return 0;
     }
